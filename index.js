f2ef308117bb143c5e6e501f05e8ae7af1b800ed
Kassie Becker <kassaundralee@hotmail.com>
2022-02-23 11:40:50 -0600

    commit tree 33c021e1de221e60dcc0a864e2f5c1f6d499215a

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-// Write your code here
+const num1 = 31
+const num2 = 2
+
+const multiply = num1 * num2
+
+const num3 = 25
+const num4 = 7
+
+const mod = num3 % num4
+
+const max = Math.max(1,3,8,20)
+
+const random = Math.trunc(Math.random()*10);
